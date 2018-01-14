5ca1ef9aacec1c80745e24bb84d6c98d871250ea
Geert Konijnendijk <geert@konijnendijk.info>
2018-01-14 17:38:15 +0100

    Fix possible crash in WorldTest
    This crash was caused by accessing the deleted entity.

--- test/src/WorldTest.cpp
+++ test/src/WorldTest.cpp
@@ -10,9 +10,18 @@ class TestEntity : public Entity {
 public:
     bool isInitialized;
     float lastTick;
+    bool* isDestructed;
 
 public:
-    TestEntity() : isInitialized(false), lastTick(-1) {}
+    TestEntity(bool* isDestructed) : isInitialized(false), lastTick(-1.0f), isDestructed(isDestructed) {}
+
+    TestEntity () : TestEntity(new bool) {
+
+    }
+
+    ~TestEntity() override {
+        *isDestructed = true;
+    }
 
     void init() override {
         isInitialized = true;
@@ -46,12 +55,14 @@ TEST_CASE("Removing entities", "[World][integration]") {
     World world;
     auto dummyEntity1 = world.createEntity<Entity>();
     auto dummyEntity2 = world.createEntity<Entity>();
-    auto removeEntity = world.createEntity<TestEntity>();
+    auto isRemoveEntityDestructed = new bool(false);
+    auto removeEntity = world.createEntity<TestEntity>(isRemoveEntityDestructed);
     auto checkEntity = world.createEntity<TestEntity>();
     world.tick(1.0);
 
     SECTION("Entity has been ticked") {
         REQUIRE(removeEntity->lastTick == 1.0);
+        REQUIRE_FALSE(*isRemoveEntityDestructed);
         REQUIRE(checkEntity->lastTick == 1.0);
     }
 
@@ -59,20 +70,22 @@ TEST_CASE("Removing entities", "[World][integration]") {
         world.removeEntity(removeEntity);
         world.tick(2.0);
 
-        REQUIRE(removeEntity->lastTick == 1.0);
+        REQUIRE(*isRemoveEntityDestructed);
         REQUIRE(checkEntity->lastTick == 2.0);
     }
 
     SECTION("Entity can be removed during the same tick") {
-        auto tickRemoveEntity = world.createEntity<TestEntity>();
+        auto isTickRemoveEntityDestructed = new bool(false);
+        auto tickRemoveEntity = world.createEntity<TestEntity>(isTickRemoveEntityDestructed);
 
         REQUIRE(tickRemoveEntity->lastTick == -1.0);
+        REQUIRE_FALSE(*isTickRemoveEntityDestructed);
         REQUIRE(checkEntity->lastTick == 1.0);
 
         world.removeEntity(tickRemoveEntity);
         world.tick(2.0);
 
-        REQUIRE(tickRemoveEntity->lastTick == -1.0);
+        REQUIRE(*isTickRemoveEntityDestructed);
         REQUIRE(checkEntity->lastTick == 2.0);
     }
 }
